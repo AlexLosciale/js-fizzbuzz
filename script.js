2ddefdd682489ac482f7ef4a6f98b3514c3094cd
AlexLosciale <losciale17@gmail.com>
2025-01-10 12:48:36 +0100

    add:finish

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
 
 for (let i = 0; i < 100; i++) {
-    if (i % 3 == 0) {
-        console.log("fizz");
+    if (i % 15 == 0) {
+        console.log("fizzbuzz");
     } else if (i % 5 == 0) {
         console.log("buzz");
-    } else if (i % 15 == 0) {
-        console.log("fizzbuzz");
+    } else if (i % 3 == 0) {
+        console.log("fizz");
     } else {
         console.log(i);
     }
